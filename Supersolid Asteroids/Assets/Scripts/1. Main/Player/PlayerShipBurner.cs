0796acf1b900273ccb591f520e582221a78c2fe3
Andrew Foster <and@rew.io>
2018-07-18 23:27:56 +0100

    Created a script to toggle the ships burner when in being moved by the player. Will look into other ways such as scrolling sprites.

---
+++ Supersolid Asteroids/Assets/Scripts/1. Main/Player/PlayerShipBurner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerShipBurner : MonoBehaviour {
+
+	//============================================================
+	// Inspector Variables:
+	//============================================================
+
+	[SerializeField] protected SpriteRenderer shipBurnerRenderer;
+
+	//============================================================
+	// Private Fields:
+	//============================================================
+
+	private bool isPlayerMoving;
+	private bool isShipBurnerEnabled;
+	private IEnumerator burnerStatusToggleEnumerator;
+
+	//============================================================
+	// Unity Lifecycle:
+	//============================================================
+
+	private void OnEnable() {
+		StartCoroutine(burnerStatusToggleEnumerator = BurnerStatusToggleCoroutine());
+	}
+
+	private void Update() {
+
+		// flag that we are moving so we can start toggling the burner on and off.
+		if (Input.GetKey(KeyCode.W)) {
+			isPlayerMoving = true;
+		}
+
+		// flag we are no-longer moving so we can prevent the burner from being toggled.
+		if (Input.GetKeyUp(KeyCode.W)) {
+			isPlayerMoving = false;
+		}
+	}
+
+	private void OnDisable() {
+		StopCoroutine(burnerStatusToggleEnumerator);
+		burnerStatusToggleEnumerator = null;
+	}
+
+	//============================================================
+	// Coroutines:
+	//============================================================
+
+	private IEnumerator BurnerStatusToggleCoroutine() {
+		while (burnerStatusToggleEnumerator != null) {
+
+			if (!isPlayerMoving) {
+				shipBurnerRenderer.enabled = false;
+				yield return new WaitForSeconds(0.1f);
+			}
+			else {
+				isShipBurnerEnabled        = !isShipBurnerEnabled;
+				shipBurnerRenderer.enabled = isShipBurnerEnabled;
+				yield return new WaitForSeconds(0.1f);
+			}
+		}
+	}
+}
