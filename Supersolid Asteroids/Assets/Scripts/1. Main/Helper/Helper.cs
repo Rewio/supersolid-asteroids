2d079aaf2e48c9efc9e0b7a606082c0f3842cacd
Andrew Foster <and@rew.io>
2018-07-22 01:30:41 +0100

    Added a custom event handler to the helper class as Unity's default event handler is a bit annoying.

--- Supersolid Asteroids/Assets/Scripts/1. Main/Helper/Helper.cs
+++ Supersolid Asteroids/Assets/Scripts/1. Main/Helper/Helper.cs
@@ -1,11 +1,16 @@
-﻿
-public static class Helper {
-	
+﻿public static class Helper {
+
 	//============================================================
 	// Decorator Variables:
 	//============================================================
 
-	public const float INSPECTOR_SPACE     = 6f;
+	public const float INSPECTOR_SPACE = 6f;
 	public const float INSPECTOR_SPACE_BIG = 10f;
+
+	//============================================================
+	// Event Handlers:
+	//============================================================
+
+	public delegate void EventHandler();
 
 }
