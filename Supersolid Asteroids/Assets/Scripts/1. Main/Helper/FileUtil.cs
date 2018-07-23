be26fdc1e4dbff5dbdacfe82c80117483fae352e
Andrew Foster <and@rew.io>
2018-07-23 21:23:18 +0100

    Initial ground work laid for reading and writing the high scores.

---
+++ Supersolid Asteroids/Assets/Scripts/1. Main/Helper/FileUtil.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class FileUtil : MonoBehaviour {
+
+	//============================================================
+	// Constants:
+	//============================================================
+
+	private const string STORAGE_FOLDER  = "Scores";
+	private const string SCORES_FILENAME = "scores.json";
+
+	//============================================================
+	// Private Fields:
+	//============================================================
+
+	private string applicationDataPath = "";
+	private string storageFolderPath   = "";
+	private string scoresFilePath      = "";
+
+	//============================================================
+	// Unity Lifecycle:
+	//============================================================
+
+	private void Start() {
+		applicationDataPath = Application.persistentDataPath;
+		storageFolderPath   = applicationDataPath + "/" + STORAGE_FOLDER;
+		scoresFilePath      = storageFolderPath + "/" + SCORES_FILENAME;
+	}
+
+	private void Update() {
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Setup();
+		}
+
+	}
+
+	//============================================================
+	// Public Methods:
+	//============================================================
+
+	public void WriteToFile(string contentsToWrite) {
+		File.WriteAllText(scoresFilePath, contentsToWrite);
+	}
+
+	public string ReadFromFile() {
+		return File.ReadAllText(scoresFilePath);
+	}
+
+	//============================================================
+	// Private Methods:
+	//============================================================
+
+	private void Setup() {
+
+		// make sure the application data path is created
+		if (!Directory.Exists(applicationDataPath)) {
+			Directory.CreateDirectory(applicationDataPath);
+		}
+
+		// ensure that we have a folder within that path for this game
+		if (!Directory.Exists(storageFolderPath)) {
+			Directory.CreateDirectory(storageFolderPath);
+		}
+
+		// and within that folder, the scores file exists for storing scores
+		if (!File.Exists(scoresFilePath)) {
+			File.Create(scoresFilePath);
+		}
+	}
+
+}
